d12b1fb3ea7762446972a82cd716a70ef82ab00f
ChadCoder39 <krolbohdan1@gmail.com>
2024-10-16 14:00:17 +0200

    Portfolio page added + minor fixes

---
+++ packages/frontend/src/app/blog/page.tsx
@@ -0,0 +1,12 @@
+import BlogBlock1 from "@/components/blog/blog1";
+import BlogBlock2 from "@/components/blog/blog2";
+
+export default function Blog() {
+    return (
+      <>
+        <BlogBlock1/>
+
+        <BlogBlock2/>
+      </>
+    );
+  }
---
+++ packages/frontend/src/app/portfolio/page.tsx
@@ -0,0 +1,12 @@
+import PortfolioBlock1 from "@/components/portfolio/pblock1";
+import PortfolioBlock2 from "@/components/portfolio/pblock2";
+
+export default function Calculate() {
+    return (
+      <>
+        <PortfolioBlock1/>
+
+        <PortfolioBlock2/>
+      </>
+    );
+  }
