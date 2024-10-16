2c81c296bfe82681984e65debf2f27f044b766a5
bada9te <bogdantelep@gmail.com>
2024-10-16 12:45:14 +0300

    ui updates for cblock2 + cblock1

--- packages/frontend/src/components/contact/cblock2.tsx
+++ packages/frontend/src/components/contact/cblock2.tsx
@@ -2,9 +2,9 @@ import ShiftedTextHeader from "../shifted-text-header";
 
 export default function ContactBlock2() {
   return (
-    <div className="relative flex flex-col lg:flex-row items-center justify-center min-h-screen p-10 text-[#322920] max-w-[1400px] mx-auto">
+    <div className="relative flex flex-col lg:flex-row items-center justify-center min-h-screen p-10 px-4 text-[#322920] max-w-[1400px] mx-auto mt-36">
       <div className="absolute top-[-50px] lg:top-[-80px] left-0 w-full text-center z-10">
-        <ShiftedTextHeader leftText="APPLY FOR A" rightText="CONSULTATION NOW" />
+        <ShiftedTextHeader leftText="APPLY FOR A" rightText="CONSULTATION NOW" customFontSize={44}/>
       </div>
 
       <div className="w-full max-w-[500px] bg-[#f5f0e1] p-8 shadow-md rounded-lg mx-auto">
--- packages/frontend/src/components/shifted-text-header.tsx
+++ packages/frontend/src/components/shifted-text-header.tsx
@@ -1,11 +1,12 @@
-export default function ShiftedTextHeader({leftText, rightText}: {
+export default function ShiftedTextHeader({leftText, rightText, customFontSize}: {
     leftText: string;
     rightText: string;
+    customFontSize?: number;
 }) {
     return (
-        <div className="flex flex-row justify-center items-center relative w-full gap-2">
-            <span className="text-[54px] absolute top-0 mr-0 md:mr-[200px]">{leftText}</span>
-            <span className="text-[54px] absolute top-14 ml-0 md:ml-[200px]">{rightText}</span>
+        <div className="flex flex-row justify-center items-center relative w-full gap-2 flex-wrap">
+            <span className={`text-[${customFontSize ? customFontSize : 54}px] absolute top-0 mr-0 md:mr-[200px]`}>{leftText}</span>
+            <span className={`text-[${customFontSize ? customFontSize : 54}px] absolute top-14 ml-0 md:ml-[200px]`}>{rightText}</span>
         </div>
     );
 }
