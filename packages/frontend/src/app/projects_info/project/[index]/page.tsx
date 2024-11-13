dd0f22df7f7ea7bfd130a32d754a588b7c166280
bada9te <bogdantelep@gmail.com>
2024-11-13 21:22:48 +0300

    updates

--- packages/frontend/src/app/projects_info/project/[index]/page.tsx
+++ packages/frontend/src/app/projects_info/project/[index]/page.tsx
@@ -3,17 +3,16 @@ import ProjectBlock2 from "@/components/projects/project_block2";
 import ProjectBlock3 from "@/components/projects/project_block3";
 import ProjectBlock4 from "@/components/projects/project_block4";
 
-export default function ProjectPage({params}: {params: {index: number}}) {
+export default function ProjectPage({params: {id}}: {params: {id: string}}) {
     return (
         <>
-          <ProjectBlock1/>
+          <ProjectBlock1 id={id}/>
     
-          <ProjectBlock2/>
+          <ProjectBlock2 id={id}/>
 
-          <ProjectBlock3/>
+          <ProjectBlock3 id={id}/>
 
           <ProjectBlock4/>
-
         </>
       );
     
--- packages/frontend/src/components/projects/project_block3.tsx
+++ packages/frontend/src/components/projects/project_block3.tsx
@@ -1,24 +1,24 @@
 import Image from "next/image";
 import data from "@/app/projects_info/project/[index]/data.json";
 
-export default function ProjectBlock3() {
-    const content = data.project_info.block3;
+export default function ProjectBlock3({id}: {id: string}) {
+    const content = data.project_info.projects.find(i => i.id == id)?.block3;
     return (
         <div className="mb-36 max-w-[1200px] mx-auto">
             
             <div className="flex flex-col lg:flex-row gap-2 mt-20 items-center justify-center p-10">
                 <div className="flex flex-col gap-4">
-                    <Image src={content.image_1} alt="serv5" width={471} height={580}/>
-                    <Image src={content.image_2} alt="serv4" width={474} height={290}/>
+                    <Image src={content?.image_1 || ""} alt="serv5" width={471} height={580}/>
+                    <Image src={content?.image_2 || ""} alt="serv4" width={474} height={290}/>
                 </div> 
 
                 <div className="flex flex-col gap-2">
-                    <Image src={content.image_3} alt="serv6" width={474} height={290}/>
-                    <Image src={content.image_4} alt="serv7" width={471} height={580}/>
+                    <Image src={content?.image_3 || ""} alt="serv6" width={474} height={290}/>
+                    <Image src={content?.image_4 || ""} alt="serv7" width={471} height={580}/>
                 </div>
 
                 <div className="flex flex-col gap-2">
-                    <Image src={content.image_5} alt="serv8" width={376} height={870}/>
+                    <Image src={content?.image_5 || ""} alt="serv8" width={376} height={870}/>
                 </div>
             </div>
         </div>
