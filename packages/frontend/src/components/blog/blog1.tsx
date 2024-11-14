70be3367c142a8f91dd2251d4a7796a82a17a516
ChadCoder39 <krolbohdan1@gmail.com>
2024-11-14 20:14:06 +0100

    Articles and blog updates

--- packages/frontend/src/components/blog/blog1.tsx
+++ packages/frontend/src/components/blog/blog1.tsx
@@ -1,7 +1,9 @@
 import ShiftedTextHeader from "../shifted-text-header";
 import Image from "next/image";
+import data from "@/app/blog/data.json";
 
 export default function BlogBlock1() {
+    const content = data.blog.block1;
     return (
         <div className="max-w-[1200px] mx-auto mb-36 mt-36">
             <div className="flex justify-center mb-60">
@@ -10,13 +12,13 @@ export default function BlogBlock1() {
 
             <div className="flex flex-col lg:flex-row items-center justify-center lg:justify-between">
                 <div className="lg:w-1/2 text-center lg:text-left lg:mr-22">
-                    <h2 className="text-4xl font-bold text-[#322920]">Blog Name</h2>
+                    <h2 className="text-4xl font-bold text-[#322920]">{content.n_1}</h2>
                     <p className="mt-4 text-lg text-[#352920]">
-                        Blog description, a few words about what this blog covers, topics discussed, and insights shared.
+                        {content.n_2}
                     </p>
                 </div>
                 <div className="lg:w-1/2">
-                    <Image src={"/assets/images/blog1.png"} className="mx-auto" width={640} height={500} alt="blog image" />
+                    <Image src={content.image_1} className="mx-auto" width={640} height={500} alt="blog image" />
                 </div>
             </div>
         </div>
--- packages/frontend/src/components/blog/blog2.tsx
+++ packages/frontend/src/components/blog/blog2.tsx
@@ -1,37 +1,39 @@
 import Image from "next/image";
+import data from "@/app/blog/data.json";
 
 export default function BlogBlock2() {
+    const content = data.blog.block2;
     return (
         <div className=" p-10 min-h-screen flex items-center justify-center">
             <div className="grid grid-cols-1 gap-10 w-full max-w-4xl">
                 <div className="flex space-x-6 justify-center">
-                <Image src={"/assets/images/blog2.png"} className="w-[570px] h-[360px]" width={500} height={500} alt="arrow"/>
+                <Image src={content.image_1} className="w-[570px] h-[360px]" width={500} height={500} alt="arrow"/>
                     <div>
-                        <h2 className="text-2xl font-semibold text-[#352920]">ARTICLE NAME</h2>
+                        <h2 className="text-2xl font-semibold text-[#352920]">{content.n_1}</h2>
                         <p className="mt-4 text-[#352920]">
-                            Description of the article, explaining the topic and addressing common questions or problems.
+                            {content.n_2}
                         </p>
                         <button className="mt-4 bg-[#352920] text-white py-2 px-6 rounded">LEARN MORE</button>
                     </div>
                 </div>
 
                 <div className="flex space-x-6 justify-center">
-                    <Image src={"/assets/images/blog3.png"} className="w-[570px] h-[360px]" width={500} height={500} alt="arrow"/>
+                    <Image src={content.image_2} className="w-[570px] h-[360px]" width={500} height={500} alt="arrow"/>
                     <div>
-                        <h2 className="text-2xl font-semibold text-[#352920]">ARTICLE NAME</h2>
+                        <h2 className="text-2xl font-semibold text-[#352920]">{content.n_3}</h2>
                         <p className="mt-4 text-[#352920]">
-                            Description of the article, explaining the topic and addressing common questions or problems.
+                            {content.n_4}
                         </p>
                         <button className="mt-4 bg-[#352920] text-white py-2 px-6 rounded">LEARN MORE</button>
                     </div>
                 </div>
 
                 <div className="flex space-x-6 justify-center">
-                <Image src={"/assets/images/blog4.png"} className="w-[570px] h-[360px]" width={500} height={500} alt="arrow"/>
+                <Image src={content.image_3} className="w-[570px] h-[360px]" width={500} height={500} alt="arrow"/>
                     <div>
-                        <h2 className="text-2xl font-semibold text-[#352920]">ARTICLE NAME</h2>
+                        <h2 className="text-2xl font-semibold text-[#352920]">{content.n_5}</h2>
                         <p className="mt-4 text-[#352920]">
-                            Description of the article, explaining the topic and addressing common questions or problems.
+                            {content.n_6}
                         </p>
                         <button className="mt-4 bg-[#352920] text-white py-2 px-6 rounded">LEARN MORE</button>
                     </div>
